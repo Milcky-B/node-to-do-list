7aca6b6edec330f2fec9c269f747435ff2b6c691
Milckyas Belay <114609852+Milcky-B@users.noreply.github.com>
2023-10-17 13:24:44 +0300

    Final touches

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,6 +2,7 @@ const sgMail = require("@sendgrid/mail");
 
 const sendAPI =process.env.SENDGRID_API_KEY
 
+const fromEmail=""
 
 
 sgMail.setApiKey(sendAPI);
@@ -9,7 +10,7 @@ sgMail.setApiKey(sendAPI);
 const createAccount = (email, name) => {
   const msg = {
     to: email,
-    from: "belaymilckyas9852@gmail.com",
+    from: fromEmail,
     subject: "Welcome to my to-do list app",
     text: `Dear ${name}, \t
     Sending this email to remind you that you created an account on my to-do list app and am really gratefull`
@@ -20,7 +21,7 @@ sgMail.send(msg)
 const deleteAccount=(email,name)=>{
   const msg={
     to:email,
-    from:"belaymilckyas9852@gmail.com",
+    from:fromEmail,
     subject:"Sad to see you go away",
     text:`Dear ${name}, \t
     Please reply to this email and tell us if there is anything you want us to improve in the future`
